92dedd413658825e9b9630d08204c335cced8e8a
vannguyen740 <dieubien8888@gmail.com>
2025-10-17 09:49:43 +0700

    Add files via upload

--- 99Night.v10.5.7.lua
+++ 99Night.v10.5.7.lua
@@ -1,38 +1,14 @@
 
-local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
-local selectedTheme = "Default"
-local Window = Rayfield:CreateWindow({
-   Name = "99 Nights In The Forest - Script By orenzoiun",
-   Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
-   LoadingTitle = "99 Nights In The Forest",
-   LoadingSubtitle = "Script By orenzoiun",
-   Theme = selectedTheme, -- Check https://docs.sirius.menu/rayfield/configuration/themes
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 
-   DisableRayfieldPrompts = false,
-   DisableBuildWarnings = false, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
-
-   ConfigurationSaving = {
-      Enabled = true,
-      FolderName = "SaverNITF", -- Create a custom folder for your hub/game
-      FileName = "K"
-   },
-
-   Discord = {
-      Enabled = false, -- Prompt the user to join your Discord server if their executor supports it
-      Invite = "noinvitelink", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ ABCD would be ABCD
-      RememberJoins = true -- Set this to false to make them join the discord every time they load it up
-   },
-
-   KeySystem = false, -- Set this to true to use our key system
-   KeySettings = {
-      Title = "Untitled",
-      Subtitle = "Key System",
-      Note = "No method of obtaining the key is provided", -- Use this to tell the user how to get a key
-      FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
-      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
-      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
-      Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
-   }
+local Window = Fluent:CreateWindow({
+    Title = "HOUGHOST Hub v10.5.7 Buy",
+    SubTitle = "orenzoiun | Discord discord./cwsTyeuB",
+    TabWidth = 180,
+    Size = UDim2.fromOffset(555, 440),
+    Acrylic = false,
+    Theme = "Darker",
+    MinimizeKey = Enum.KeyCode.End
 })
 local InfoTab = Window:CreateTab("Info")
 local PlayerTab = Window:CreateTab("Player")
@@ -1086,7 +1062,7 @@ while true do
 task.spawn(function()
 	local updatedInfo = getServerInfo()
 	local updatedContent = string.format(
-		"📌 PlaceId: %s\n🔑 JobId: %s\n🧪 IsStudio: %s\n👥 Players: %d/%d - 👋Hello everyone, today everyone comes to orenzoiun's script. Wish everyone a happy script.",
+		"📌 PlaceId: %s\n🔑 JobId: %s\n🧪 IsStudio: %s\n👥 Players: %d/%d",
 		updatedInfo.PlaceId,
 		updatedInfo.JobId,
 		
@@ -1096,7 +1072,7 @@ updatedInfo.MaxPlayers
 	)
 
 	ParagraphInfoServer:Set({
-		Title = "Info - Welcome",
+		Title = "Info",
 		Content = updatedContent
 	})
 end)
